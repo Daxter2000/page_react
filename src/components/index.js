707daffe23e52349c1447e1948b886ccfb61ed27
Alfredo Hernandez <alfredo.hdez@hotmail.com>
2022-11-28 15:05:15 -0700

    Creating finals components

---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import CTA from "./CTA";
+import Clients from "./Clients";
+import FeedbackCard from "./FeedbackCard";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import Navbar from "./Navbar";
+
+
+export {
+    Billing,
+    Business,
+    Button,
+    CardDeal,
+    CTA,
+    Clients,
+    FeedbackCard,
+    Footer,
+    GetStarted,
+    Hero,
+    Stats,
+    Testimonials,
+    Navbar
+};
